ae9ff575548a68b7b2eda00a23d29ac88379ffce
DJWOMS <socanime@gmail.com>
2020-11-14 23:46:25 +0300

    add auth service, layout login, logic auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../environments/environment';
+import {Observable} from 'rxjs';
+import {Login, Registration, Token} from './models';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(model: Login): Observable<Token> {
+    return this.http.post<Token>(`${environment.url}/auth/jwt/create/`, model);
+  }
+
+}
